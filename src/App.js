4d618abbe269925c0aa9ae05e42dd97407ea4e37
JXApple <2477393683@qq.com>
2021-03-07 17:50:01 +0800

    最原始create-react-app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/*
+ * @description  :
+ * @Author       : zengrongping
+ * @Date         : 2021-03-07 17:43:10
+ * @LastEditors  : zengrongping
+ * @LastEditTime : 2021-03-07 17:48:15
+ * @FilePath     : \react-mobile-create\src\App.js
+ */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
